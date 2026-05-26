a4ad0ae237112f84ba52d77df35e475e94b919f3
Piotr Kowalski <piotrkow93@gmail.com>
2026-05-26 23:05:14 +0200

    Starting benchmarks in .net

---
+++ src/VectorStruct/Benchmarks.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+
+namespace VectorStruct
+{
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public void ScalarSum()
+        {
+            int sum = 0;
+            int[] array = new int[1000000];
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                sum += i;
+            }
+        }
+        
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}
