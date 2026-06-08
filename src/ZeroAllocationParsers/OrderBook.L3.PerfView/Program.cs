eb2f2eed866354252ece76639782a99ec1afc42a
Piotr Kowalski <piotrkow93@gmail.com>
2026-06-08 23:07:48 +0200

    Next Version OrderBook with pointers

--- src/ZeroAllocationParsers/OrderBook.L3.PerfView/Program.cs
+++ src/ZeroAllocationParsers/OrderBook.L3.PerfView/Program.cs
@@ -8,24 +8,24 @@ internal class Program
         Console.ReadLine();
 
         List<OrderEvent> _events;
-        OrderBook.L3.OrderBook_v1 _book;
-        OrderManager_v1 _manager;
+        OrderBook.L3.OrderBook _book;
+        OrderManager _manager;
 
         _events = CsvFeedReader
                     .Read("Feed.csv")
                     .ToList();
 
         // Break to run profiler before processing events
-        Console.WriteLine("READY");
-        Console.ReadLine();
+        //Console.WriteLine("READY");
+        //Console.ReadLine();
 
-        _book = new OrderBook.L3.OrderBook_v1("Sample");
-        _manager = new OrderManager_v1(_book);
+        _book = new OrderBook.L3.OrderBook("Sample");
+        _manager = new OrderManager(_book);
 
-        foreach (var evt in _events)
-        {
-            _manager.Process(evt);
-        }
+        //foreach (var evt in _events)
+        //{
+        //    _manager.Process(evt);
+        //}
 
         Console.WriteLine("END");
         Console.ReadLine();
---
+++ src/ZeroAllocationParsers/OrderBook.L3/OrderBook.cs
@@ -0,0 +1,76 @@
+﻿namespace OrderBook.L3
+{
+    public unsafe struct OrderBook
+    {
+        private const int MAX_ORDERS = 1_500_000;
+        private const int MAX_PRICE_LEVELS = 1_000;
+
+        private string _tick;
+
+        private PriceLevel* ask_PriceLevels_Head_;
+        private PriceLevel* bid_PriceLevels_Head_;
+
+        private Order*[] orders_;
+        private PriceLevel*[] priceLevels_;
+
+        private long OrderIdToIndex(long orderId)
+        {
+            return orderId % MAX_ORDERS;
+        }
+        
+        private int PriceToIndex(int price)
+        {
+            return price % MAX_PRICE_LEVELS;
+        }
+
+        public OrderBook(string tick)
+        {
+            _tick = tick;
+            orders_ = new Order*[MAX_ORDERS];
+            priceLevels_ = new PriceLevel*[MAX_PRICE_LEVELS];
+        }
+
+        public void Add(long OrderId, int Side, int Price, int Quantity)
+        {
+        }
+
+        public void Cancel(long orderId)
+        {
+        }
+
+        public void Modify(long orderId, int newQuantity)
+        {
+        }
+
+        public void Execute(long orderId, int executedQuantity)
+        {
+        }
+    }
+
+    public unsafe struct Order
+    {
+        public long OrderId { get; set; }
+        public int Side { get; set; }       // 0 for buy, 1 for sell
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Order* prev_;
+        public Order* next_;
+        
+        public PriceLevel* PriceLevel;
+    }
+
+    public unsafe struct PriceLevel
+    {
+        public int Price { get; set; }
+        public int Side { get; set; }       // 0 for buy, 1 for sell
+        public int TotalQuantity { get; set; }
+        public int OrdersCount { get; set; }
+
+        public Order* FirstOrder;
+        public Order* LastOrder;
+
+        public PriceLevel* PriceLevelPrev;
+        public PriceLevel* PriceLevelNext;
+    }
+}
